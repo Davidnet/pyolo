0ede831afb7ac96cb113a73cbcfc23bccbab0b30
charlielito <charlie@kiwicampus.com>
2018-04-25 21:22:37 -0700

    make it module

--- object_detection.py
+++ object_detection.py
@@ -1,15 +1,56 @@
 """
 File that contains the class for initialazing the fast object detector
 """
-import sys, os
+
+import sys, os, shutil, subprocess
 import numpy as np
 import pyyolo
 import cv2
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+URL_PATH = "gs://vision-198622-production-models/object-detection/tiny-yolo/tiny-yolo.weights"
 
 class TinyYOLODetector(object):
-    def __init__(self, thresh=0.24, hier_thresh=0.5):
+    """Class detection for TinyYolo"""
+    @staticmethod
+    def get(url=URL_PATH, *args, **kwargs):
+        rm = kwargs.pop("rm", False)
+
+        hash_name = str(hash(url)).replace("-", "0")
+        filename = os.path.basename(url)
+
+        home_path = os.path.expanduser('~')
+        model_dir_base = os.path.join(home_path, ".local", "tfinterface", "frozen_graphs", hash_name)
+        model_path = os.path.join(model_dir_base, filename)
+
+        if os.path.exists(model_dir_base):
+
+            is_empty = len(os.listdir(model_dir_base)) == 0
+            files_counts = [ os.path.join(dp, f) for dp, _, filenames in os.walk(model_dir_base) for f in filenames if f.endswith(".gstmp") ]
+            temp_files = len(files_counts) > 0
+
+            if rm or is_empty or temp_files:
+                shutil.rmtree(model_dir_base, ignore_errors = True)
+
+        if not os.path.exists(model_dir_base):
+            os.makedirs(model_dir_base)
+
+            cmd = "gsutil -m cp -R {source_folder} {dest_folder}".format(
+                source_folder = url,
+                dest_folder = model_dir_base,
+            )
+
+            # print("CMD: {}".format(cmd))
+
+            subprocess.check_call(
+                cmd,
+                stdout = subprocess.PIPE, shell = True,
+            )
+    return YOLODetector(weightfile, *args, **kwargs)
+
+class YOLODetector(object):
+    """Class detection for YOLO objects in general"""
+    def __init__(self, weightfile, thresh=0.24, hier_thresh=0.5):
         #darknet_path = './darknet'
         darknet_path = os.path.join(DIR_PATH, "darknet")
         #datacfg = 'cfg/coco.data'
@@ -17,10 +58,11 @@ class TinyYOLODetector(object):
         #cfgfile = 'cfg/tiny-yolo.cfg'
         cfgfile = os.path.join(darknet_path, "cfg", "tiny-yolo.cfg")
         #weightfile = '../tiny-yolo.weights'
-        weightfile = os.path.join(DIR_PATH, "tiny-yolo.weights")
         self.thresh = thresh
         self.hier_thresh = hier_thresh
         pyyolo.init(darknet_path, datacfg, cfgfile, weightfile)
+
+
 
     def _parser_raw_output(self, raw_output):
         """
